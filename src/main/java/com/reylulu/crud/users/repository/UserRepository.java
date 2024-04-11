a800525ac3ff8b89cd72562094d51fa320cd72c1
Rey Lulu <mr21082@ues.edu.sv>
2024-04-10 23:00:52 -0600

    Subiendo fuentes de aplicacion

---
+++ src/main/java/com/reylulu/crud/users/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.reylulu.crud.users.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reylulu.crud.users.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+
+}
